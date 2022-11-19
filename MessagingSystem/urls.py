e9ac1d9c92b3ac988c43b047ea81835a49297cf4
Matan Peretz <matanperetz@outlook.com>
2022-11-19 14:48:28 +0200

    add Login option

--- MessagingSystem/urls.py
+++ MessagingSystem/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from messages_api import urls as messages_urls
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('api-auth/', include('rest_framework.urls')),
     path('messages/', include(messages_urls)),
-
+    path('login', users_views.LoginView.as_view()),
 ]
